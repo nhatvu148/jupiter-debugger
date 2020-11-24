4a7c7189d9eac29effe06a4451d336a4eca76dd2
nhatvu148 <nhatvu148@gmail.com>
2020-11-24 22:38:58 +0900

    adding wrapper

---
+++ Jupiter.py
@@ -0,0 +1,17 @@
+class AbaqusStep:
+    def __init__(self):
+        pass
+    def DynamicStep(self):
+        return 1234
+
+class Analysis:
+    B = 5
+    AbaqusStep = AbaqusStep()
+    def Abaqus(c):
+        return "Hello" + c
+
+class JPT:
+    def RemoveEntitiesByID():
+        return "RemoveEntitiesByID"
+    def RemoveEntitiesByName():
+        return "RemoveEntitiesByName"
