349e91d8ceb82e938dfd21e908f0832bce9d854a
nhatvu148 <nhatvu148@gmail.com>
2020-11-24 23:50:18 +0900

    test doc string

--- Jupiter.py
+++ Jupiter.py
@@ -11,7 +11,35 @@ class Analysis:
         return "Hello" + c
 
 class JPT:
+    """
+    Function: JPT.RemoveAllLoadCases
+    Description: Remove all load cases in models
+    Input1: None
+    Return: None
+    Example: JPT.RemoveAllLoadCases()
+    """
     def RemoveEntitiesByID():
         return "RemoveEntitiesByID"
     def RemoveEntitiesByName():
         return "RemoveEntitiesByName"
+    def RemoveAllLoadsBCs():
+        return "RemoveAllLoadsBCs"
+    def RemoveAllContacts():
+        return "RemoveAllContacts"
+    def RemoveAllConnections():
+        return "RemoveAllConnections"
+    def RemoveAllLoadCases():
+        return "RemoveAllLoadCases"
+    def RemoveAllMaterials():
+        return "RemoveAllMaterials"
+    def RemoveWSProperties():
+        return "RemoveWSProperties"
+    def RemoveAllCoordinates():
+        return "RemoveAllCoordinates"
+    def RemoveAllMeshSettings():
+        return "RemoveAllMeshSettings"
+    def RemoveAllFieldTables():
+        return "RemoveAllFieldTables"
+    def RemoveAllAbaqusStep():
+        return "RemoveAllAbaqusStep"
+
